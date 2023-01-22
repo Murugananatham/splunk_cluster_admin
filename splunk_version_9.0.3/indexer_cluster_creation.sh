3d701ccedf7189e0f2d4d4ba3293c2220616f6bd
Muruganantham Pothanaickar <murugananthamp0104@gmail.com>
2023-01-22 13:13:13 +0530

    CLI Command arguments update

--- splunk_version_9.0.3/indexer_cluster_creation.sh
+++ splunk_version_9.0.3/indexer_cluster_creation.sh
@@ -1,6 +1,8 @@
+### Execute below commands as a Splunk User
+
 ########## In Cluster Manager
 
-/opt/splunk/bin/splunk edit cluster-config -mode manager -replication_factor 2 -search_factor 2 -secret INDEXER_CLUSTER_SECRET_KEY -cluster_label INDEXER_CLUSTER_LABEL
+/opt/splunk/bin/splunk edit cluster-config -mode manager -replication_factor 2 -search_factor 2 -secret INDEXER_CLUSTER_SECRET_KEY -cluster_label INDEXER_CLUSTER_LABEL -auth admin:PASSWORD
 
 /opt/splunk/bin/splunk set servername SPLK-ClusterMaster -auth admin:PASSWORD
 /opt/splunk/bin/splunk set default-hostname SPLK-ClusterMaster -auth admin:PASSWORD
@@ -13,7 +15,7 @@
 ########## In Indexers 
 
 # In Indexer 1
-/opt/splunk/bin/splunk edit cluster-config -mode peer -manager_uri https://CLUSTER_MASTER_NODE_FQDN:8089 -replication_factor 9000 -secret INDEXER_CLUSTER_SECRET_KEY
+/opt/splunk/bin/splunk edit cluster-config -mode peer -manager_uri https://CLUSTER_MASTER_NODE_FQDN:8089 -replication_port 9000 -secret INDEXER_CLUSTER_SECRET_KEY -auth admin:PASSWORD
 
 /opt/splunk/bin/splunk set servername SPLK-Indexer-1 -auth admin:PASSWORD
 /opt/splunk/bin/splunk set default-hostname SPLK-Indexer-1 -auth admin:PASSWORD
@@ -22,7 +24,7 @@
 
 
 # In Indexer 2
-/opt/splunk/bin/splunk edit cluster-config -mode peer -manager_uri https://CLUSTER_MASTER_NODE_FQDN:8089 -replication_factor 9000 -secret INDEXER_CLUSTER_SECRET_KEY
+/opt/splunk/bin/splunk edit cluster-config -mode peer -manager_uri https://CLUSTER_MASTER_NODE_FQDN:8089 -replication_port 9000 -secret INDEXER_CLUSTER_SECRET_KEY -auth admin:PASSWORD
 
 /opt/splunk/bin/splunk set servername SPLK-Indexer-2 -auth admin:PASSWORD
 /opt/splunk/bin/splunk set default-hostname SPLK-Indexer-2 -auth admin:PASSWORD
@@ -31,7 +33,7 @@
 
 
 # In Indexer 3
-/opt/splunk/bin/splunk edit cluster-config -mode peer -manager_uri https://CLUSTER_MASTER_NODE_FQDN:8089 -replication_factor 9000 -secret INDEXER_CLUSTER_SECRET_KEY
+/opt/splunk/bin/splunk edit cluster-config -mode peer -manager_uri https://CLUSTER_MASTER_NODE_FQDN:8089 -replication_port 9000 -secret INDEXER_CLUSTER_SECRET_KEY -auth admin:PASSWORD
 
 /opt/splunk/bin/splunk set servername SPLK-Indexer-3 -auth admin:PASSWORD
 /opt/splunk/bin/splunk set default-hostname SPLK-Indexer-3 -auth admin:PASSWORD
